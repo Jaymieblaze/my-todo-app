884c983504ddc225f610566a0b8bc26ab68f35cd
Jaymieblaze <jameseyiaro@gmail.com>
2025-09-22 11:29:16 +0100

    Add Firebase integration and update dependencies
    - Added Firebase SDK as a dependency in package.json.
    - Created firebase.ts to initialize Firebase with configuration settings and set up Firebase Authentication.

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add your own Firebase configuration from your Firebase project settings
+// IMPORTANT: Replace this with the firebaseConfig object you copied from the Firebase console
+const firebaseConfig = {
+  apiKey: "AIzaSyAhhEtueD44Y1npjPtJ3_3nDTbyMnLvf9Y",
+  authDomain: "mytodoapp-f521e.firebaseapp.com",
+  projectId: "mytodoapp-f521e",
+  storageBucket: "mytodoapp-f521e.firebasestorage.app",
+  messagingSenderId: "48707429602",
+  appId: "1:48707429602:web:c6a27c5221188256c56abb",
+  measurementId: "G-08Y48QRF4J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
